e2738da9890e9c886bf76c9a25e5720a3145464f
JinyuSun <jinyusun@csu.edu.cn>
2023-09-02 12:54:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ st.title("OSC Database")
 
 # Connect to the Google Sheet
 url = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=accept"
+url1 = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=111"
 df = pd.read_csv(url, dtype=str, encoding='utf-8')
+df1 = pd.read_csv(url1, dtype=str, encoding='utf-8')
 st.write(df)
+st.write(df1)
