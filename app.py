f320e27b41a322d9598eac109b8585113061d290
JinyuSun <jinyusun@csu.edu.cn>
2023-09-02 13:25:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ url = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0
 url1 = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=111"
 df = pd.read_csv(url, dtype=str, encoding='utf-8')
 df1 = pd.read_csv(url1, dtype=str, encoding='utf-8')
-edited_df = st.data_editor(df)
-edited_df = st.data_editor(df, num_rows="dynamic")
-st.data_editor(df, key="data_editor") 
-st.write("Here's the session state:")
-st.write(st.session_state["data_editor"])
-st.write(df,df1)
+edited_df = st.data_editor(df1, num_rows="dynamic")
+st.write(df1)
 
