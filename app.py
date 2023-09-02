e90f7ac2976e9d7272a9eccc03f2c86516368562
JinyuSun <jinyusun@csu.edu.cn>
2023-09-02 11:16:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+url = f"https://docs.google.com/spreadsheets/d/1x1z3l-Hxcx5mwkTYM_AVaqzHt7nBfOOWQphnaoTAkLY/edit?usp=sharing"
+df = pd.read_csv(url, dtype=str)
+import streamlit as st
+import pandas as pd
 
+# Page setup
+st.set_page_config(page_title="Python Talks Search Engine", page_icon="🐍", layout="wide")
+st.title("Python Talks Search Engine")
+
+# Connect to the Google Sheet
+sheet_id = "1nctiWcQFaB5UlIs6z8d1O6ZgMHFDMAoo3twVxYnBUws"
+sheet_name = "charlas"
+url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
+df = pd.read_csv(url, dtype=str).fillna("")
+
+# Show the dataframe (we'll delete this later)
+st.write(df)
