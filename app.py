4ea8b04c02ec47f3c152807df59d3aa86ca7ff52
JinyuSun <jinyusun@csu.edu.cn>
2023-09-02 11:30:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,5 @@ st.title("Python Talks Search Engine")
 
 # Connect to the Google Sheet
 url = f"https://docs.google.com/spreadsheets/d/1x1z3l-Hxcx5mwkTYM_AVaqzHt7nBfOOWQphnaoTAkLY/edit?usp=sharing"
-df = pd.read_csv(url, dtype=str)
-
-# Show the dataframe (we'll delete this later)
+df = pd.read_csv(url, dtype=str, delimiter = '\t')
 st.write(df)
