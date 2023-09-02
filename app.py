2c6729ebf8ffe1d0175305e6916954a88f59e036
JinyuSun <jinyusun@csu.edu.cn>
2023-09-02 13:02:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ url1 = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J
 df = pd.read_csv(url, dtype=str, encoding='utf-8')
 df1 = pd.read_csv(url1, dtype=str, encoding='utf-8')
 q= list([df,df1])
-st.write(a)
+st.write(q)
 
