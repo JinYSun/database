6330609b8cf799c4054a769ebe0e41cb83674758
JinyuSun <jinyusun@csu.edu.cn>
2023-09-16 16:48:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import streamlit as st
 import pandas as pd
- 
+import rdkit
 import streamlit_ketcher
 from streamlit_ketcher import st_ketcher
+import abcBERT
 
 # Page setup
-st.set_page_config(page_title="Non-fullere acceptor", page_icon="🔋", layout="wide")
-st.title("OSC Database")
+st.set_page_config(page_title="DeepAcceptor", page_icon="🔋", layout="wide")
+st.title("DeepAcceptor")
 
 # Connect to the Google Sheet
-url = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=accept"
-url1 = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=111"
-df = pd.read_csv(url, dtype=str, encoding='utf-8')
+url1 = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=accept"
+url = r"https://docs.google.com/spreadsheets/d/1YOEIg0nMTSPkAOr8wkqxQRLuUhys3-J0I-KPEpmzPLw/gviz/tq?tqx=out:csv&sheet=111"
 df1 = pd.read_csv(url1, dtype=str, encoding='utf-8')
+
+text_search = st.text_input("Search papers or molecules", value="")
+m1 = df1["name"].str.contains(text_search)
+m2 = df1["reference"].str.contains(text_search)
+df_search = df1[m1 | m2]
+if text_search:
+    st.write(df_search)
+    st.download_button( "Download edited files as .csv", df_search.to_csv(), "df_search.csv", use_container_width=True)
 edited_df = st.data_editor(df1, num_rows="dynamic")
+edited_df.to_csv(url)
 st.download_button(
     "⬇️ Download edited files as .csv", edited_df.to_csv(), "edited_df.csv", use_container_width=True
 )
@@ -21,3 +30,8 @@ st.download_button(
 molecule = st.text_input("Molecule")
 smile_code = st_ketcher(molecule)
 st.markdown(f"Smile code: ``{smile_code}``")
+try:
+    pce = abcBERT.main( str(smile_code ) )
+    st.markdown(f"PCE: ``{pce}``")
+except:
+    st.markdown(f"PCE:  None  ")
